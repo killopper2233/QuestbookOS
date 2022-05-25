db82fe96b9c4cd2f7f44fa31d0c0f56eb9834c37
Taishan Dixon <61961329+killopper2233@users.noreply.github.com>
2022-05-25 01:10:30 +0000

    Added even more apps. Lockscreen is still in development!

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+/* load the server */
 const express = require('express');
 
 const app = express();
 
+
+app.use("/apps",express.static("apps"));
+app.use("/QDE", express.static("QuestbookOS Desktop Environment"))
+app.use("/src", express.static("Finished loading important files!"))
+
 app.get('/', function(req, res) {
+  res.setHeader('Content-Type', 'text/html');
   res.sendFile('index.html',  { root: __dirname });
+    
 });
 
 app.listen(3000, () => {
-  console.log('server started');
+  console.log('QuestbookOS started');
 });
+// start the JS server
+// ignore when OS is finshed
